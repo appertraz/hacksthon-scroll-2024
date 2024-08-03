dc40a0d66dbf8e592bcc0fa5ac66fe0fc0e9c0a4
Schmidt Cristian Hernán <schcriher@gmail.com>
2024-08-03 06:24:17 -0300

    add backend mock structure

---
+++ backend.js
@@ -0,0 +1,29 @@
+/**
+ * Diego aca voy a ir implementando las funciones que emularan el backend
+ *
+ * TODO: Despues definimos ls forma de exportar las funciones en funcion de como lo necesites
+ */
+
+export async function newClient() {
+  //
+  // TODO: implement
+  //
+}
+
+export async function listClients() {
+  //
+  // TODO: implement
+  //
+}
+
+export async function newLot() {
+  //
+  // TODO: implement
+  //
+}
+
+export async function listLots() {
+  //
+  // TODO: implement
+  //
+}

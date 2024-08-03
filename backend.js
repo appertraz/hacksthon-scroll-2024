2d5ee7a4969e964d63c5e04cdadabedc77db4bd4
Schmidt Cristian Hernán <schcriher@gmail.com>
2024-08-03 20:39:20 -0300

    basic backend in localstorage

--- backend.js
+++ backend.js
@@ -5,7 +5,7 @@ function serializer(key, value) {
   return value;
 }
 
-export default function useStorage(type = "local") {
+function useStorage(type = "local") {
   const storage = `${type}Storage`; /* local | session */
 
   const toStore = (obj) => JSON.stringify(obj, serializer);
@@ -35,9 +35,8 @@ function getID() {
 //-------------------------------------
 
 /*
-DB FORMAT:
----------
-
+DB EXAMPLE:
+----------
 const clients = [
   {
     id: 0,
@@ -47,6 +46,7 @@ const clients = [
       {
         id: 0,
         name: "name",
+        status: "loading", // loading | minting | finished
         description: "description",
         nft: "0x378E58E2033dD69927398b1979dD39eFc3123a43",
       },
@@ -55,26 +55,87 @@ const clients = [
 ];
 */
 
-export async function newClient(nombre) {
-  //
-  // TODO: implement, return ID
-  //
+export function listClients() {
+  return DB.get("clients") || [];
 }
 
-export async function listClients() {
-  //
-  // TODO: implement
-  //
+export function newClient(name) {
+  const clients = DB.get("clients") || [];
+  clients.push({
+    id: getID(),
+    name: name,
+    minter: null,
+    lots: [],
+  });
+  DB.set("clients", clients);
 }
 
-export async function newLot(clientId, name, description) {
-  //
-  // TODO: implement, return ID
-  //
+function setClient(clientId, field, value) {
+  const clients = DB.get("clients") || [];
+  const filteredClient = clients.filter((client) => client.id === clientId);
+  if (filteredClient.length === 0) {
+    throw new Error("Client not found");
+  }
+  filteredClient[0][field] = value;
+  DB.set("clients", clients);
 }
 
-export async function listLots() {
-  //
-  // TODO: implement
-  //
+export function setClientMinter(clientId, minter) {
+  return setClient(clientId, "minter", minter);
+}
+
+//-------------------------------------
+
+export function listLots(clientId) {
+  const clients = DB.get("clients") || [];
+  const filteredClient = clients.filter((client) => client.id === clientId);
+  if (filteredClient.length === 0) {
+    throw new Error("Client not found");
+  }
+  return filteredClient[0].lots;
+}
+
+export function newLot(clientId, name) {
+  const clients = DB.get("clients") || [];
+  const filteredClient = clients.filter((client) => client.id === clientId);
+  if (filteredClient.length === 0) {
+    throw new Error("Client not found");
+  }
+  filteredClient[0].lots.push({
+    id: getID(),
+    name: name,
+    status: "loading",
+    description: null,
+    nft: null,
+  });
+  DB.set("clients", clients);
+}
+
+function setLot(clientId, lotId, field, value) {
+  const clients = DB.get("clients") || [];
+  const filteredClient = clients.filter((client) => client.id === clientId);
+  if (filteredClient.length === 0) {
+    throw new Error("Client not found");
+  }
+  const filteredLot = filteredClient[0].lots.filter((lot) => lot.id === lotId);
+  if (filteredLot.length === 0) {
+    throw new Error("Lot not found");
+  }
+  filteredLot[0][field] = value;
+  DB.set("clients", clients);
+}
+
+export function setLotDescription(clientId, lotId, description) {
+  return setLot(clientId, lotId, "description", description);
+}
+
+export function setLotNft(clientId, lotId, nft) {
+  return setLot(clientId, lotId, "nft", nft);
+}
+
+export function setLotStatus(clientId, lotId, status) {
+  if (!["loading", "minting", "finished"].includes(status)) {
+    throw new Error("Invalid status");
+  }
+  return setLot(clientId, lotId, "status", status);
 }
